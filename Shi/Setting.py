2c5946a994a123d409f3679e35dc475c5139225e
beakerThe2nd <158429694+beakerThe2nd@users.noreply.github.com>
2024-07-11 19:45:14 -0700

    change

--- Shi/Setting.py
+++ Shi/Setting.py
@@ -5,4 +5,6 @@ class welcomeScreen():
  def __init__(self) -> None:
     print("Please enter the wavelengths and the powers with other information necessary into the txt file 'parameters' within the same folder")
     print("Press 'enter' when you are ready")
-    parameters = open("parameters.txt")
+    file = open("parameters.txt")
+    parameters = file.readlines()
+    
