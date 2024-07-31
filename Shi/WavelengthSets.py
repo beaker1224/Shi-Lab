1478256baba624bd13a3a1fa81c0e9380cee26b4
beakerThe2nd <158429694+beakerThe2nd@users.noreply.github.com>
2024-07-31 14:00:54 -0700

    setting

---
+++ Shi/WavelengthSets.py
@@ -0,0 +1,12 @@
+import pyautogui
+import sys, time
+class wavelengthSet(self):
+    def __init__(self, order, wavelength, power, IR_status, average, zoom)
+
+    def naming(self):
+        order = str(self.order)
+        wavelength = str(self.wavelength)
+        power = str(self.power)
+        average = str(self.average)
+        name = order + '-' + wavelength + 'nm-' + power + 'mW-' + 
+        pyautogui.typewrite(name, interval=secs_between_keys)
