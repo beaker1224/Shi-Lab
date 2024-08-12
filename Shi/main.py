7419b93fefddea44f88d5aad98449b3fdc8a0d5b
beakerThe2nd <158429694+beakerThe2nd@users.noreply.github.com>
2024-08-12 14:46:03 -0700

    main1

--- Shi/main.py
+++ Shi/main.py
@@ -4,9 +4,21 @@ import parameter_interpreter_3
 import parameter_sets
 import pico_emeraldWatch_1
 
+def load_from_json(file_name):
+    with open(file_name, 'r') as file:
+        return json.load(file)
+
 def main():
     # interprete the data in parameters.txt
     parameter_interpreter_3.interpreter()
-    
+    parameters = load_from_json("parameters.json")
+    pico_emerald = load_from_json("pico_emerald_layout.json")
+    FV = load_from_json("FV_layout.json")
+
+    while true:
+        i = 0
+        parameters = load_from_json("parameters.json")
+
+        while i < len()
 
 main()
