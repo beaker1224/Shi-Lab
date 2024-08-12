3a772d19e2ccba8dbd4f54e47e0744662eaee94c
beakerThe2nd <158429694+beakerThe2nd@users.noreply.github.com>
2024-08-12 15:15:54 -0700

    setter update

--- Shi/main.py
+++ Shi/main.py
@@ -10,21 +10,33 @@ def load_from_json(file_name):
         return json.load(file)
 
 def main():
+    order = input("which order (#th) roi are you taking image of? number only!")
     # interprete the data in parameters.txt
     parameter_interpreter_3.interpreter()
-    
+
     parameters = load_from_json("parameters.json")
     wavelengths = tuple(parameters['wavelength'])
     powers = tuple(parameters['power'])
     averages = tuple(parameters['average'])
     channels = tuple(parameters['channel'])
-    
+    i = 0
+    while i < len(wavelengths):
+        sets = []
+        sets.append(inputSet(order, wavelengths[i], powers[i], averages[i], channels[i]))
+    print(sets)
+
     pico_emerald_layout = load_from_json("pico_emerald_layout.json")
+
+
     FV_layout = load_from_json("FV_layout.json")
 
-    while true:
-        i = 0
+    input("IMPORTANT: set the wavelength and power to the first set you need to take!!!!!!!!!!!!!!!!!")
 
+    input("Press 'enter' to start the auto lsm system")
+
+    while true:
+        
+        i = 1
         while i < len(wavelengths):
             pico_emeraldWatch_1.change_wavelength_to(wavelengths[i])
 
