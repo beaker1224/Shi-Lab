d3b24c944af190f8e33f90f7a154e2aad1ba9284
beakerThe2nd <158429694+beakerThe2nd@users.noreply.github.com>
2024-08-12 14:14:06 -0700

    test pass one

--- Shi/pico_emeraldWatch_1.py
+++ Shi/pico_emeraldWatch_1.py
@@ -76,33 +76,41 @@ def main():
     input("display for information, press 'enter' when you want to exist and finish updating pico emerald layout setting")
 
 def position_getter():
-        config = load_from_json("pico_emerald_layout.json")
-        shutter_position = tuple(config['shutter_position'])
-        shutter_on = tuple(config['shutter_on_color'])
-        IR_position = tuple(config['IR_position'])
-        IR_on = tuple(config['IR_on_color'])
-        wavelength_position = tuple(config['wavelength_position'])
-        power_position = tuple(config['power_position'])
+    config = load_from_json("pico_emerald_layout.json")
+    shutter_position = tuple(config['shutter_position'])
+    shutter_on = tuple(config['shutter_on_color'])
+    IR_position = tuple(config['IR_position'])
+    IR_on = tuple(config['IR_on_color'])
+    wavelength_position = tuple(config['wavelength_position'])
+    power_position = tuple(config['power_position'])
 
 def turn_off_IR():
-    position_getter()
+    config = load_from_json("pico_emerald_layout.json")
+    IR_position = tuple(config['IR_position'])
+    IR_on = tuple(config['IR_on_color'])
     if tuple(get_pixel_color(*IR_position)) == IR_on:
         pyautogui.click(IR_position)
 
 def change_wavelength_to(wavelength):
-    position_getter()
+    config = load_from_json("pico_emerald_layout.json")
+    wavelength_position = tuple(config['wavelength_position'])
     pyautogui.click(wavelength_position)
+    pyautogui.typewrite(['backspace','backspace','backspace','backspace','backspace'], interval = 0.2)
     pyautogui.typewrite(wavelength)
     pyautogui.PAUSE = 0.5
     pyautogui.typewrite('enter')
 
 def change_power_to(power):
-    position_getter()
+    config = load_from_json("pico_emerald_layout.json")
+    power_position = tuple(config['power_position'])
     pyautogui.click(power_position)
-    pyautogui.typewrite(power)
+    pyautogui.typewrite(['backspace','backspace','backspace','backspace','backspace'], interval = 0.2)
+    pyautogui.typewrite(str(power))
     pyautogui.PAUSE = 0.5
-    pyautogui.typewrite('enter')
+    pyautogui.typewrite(['enter'])
 
+change_power_to(300)
+pyautogui.typewrite(1234)
 # this will make sure when the py script is called directly, the above function will run
 if __name__ == "__main__":
     main()
