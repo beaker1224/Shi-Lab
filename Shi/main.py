81b649d2978098b4e2b0fe440354a823aad2269b
beakerThe2nd <158429694+beakerThe2nd@users.noreply.github.com>
2024-08-12 15:02:28 -0700

    main update 1

--- Shi/main.py
+++ Shi/main.py
@@ -3,6 +3,7 @@ import os
 import parameter_interpreter_3
 import parameter_sets
 import pico_emeraldWatch_1
+import time
 
 def load_from_json(file_name):
     with open(file_name, 'r') as file:
@@ -11,14 +12,20 @@ def load_from_json(file_name):
 def main():
     # interprete the data in parameters.txt
     parameter_interpreter_3.interpreter()
+    
     parameters = load_from_json("parameters.json")
-    pico_emerald = load_from_json("pico_emerald_layout.json")
-    FV = load_from_json("FV_layout.json")
+    wavelengths = tuple(parameters['wavelength'])
+    powers = tuple(parameters['power'])
+    averages = tuple(parameters['average'])
+    channels = tuple(parameters['channel'])
+    
+    pico_emerald_layout = load_from_json("pico_emerald_layout.json")
+    FV_layout = load_from_json("FV_layout.json")
 
     while true:
         i = 0
-        parameters = load_from_json("parameters.json")
 
-        while i < len()
+        while i < len(wavelengths):
+            pico_emeraldWatch_1.change_wavelength_to(wavelengths[i])
 
 main()
