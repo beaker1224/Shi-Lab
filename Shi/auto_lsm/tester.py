0c641265e5555ade113d5d1f33ef350928337d33
beaker1224 <87831973+beaker1224@users.noreply.github.com>
2024-08-13 17:14:37 -0700

    tester updated

---
+++ Shi/auto_lsm/tester.py
@@ -0,0 +1,11 @@
+import pyautogui
+import os
+import parameter_interpreter_3
+import parameter_sets
+import pico_emeraldWatch_1
+import time
+import json
+
+pico_emeraldWatch_1.change_power_to("250")
+time.sleep(0.5)
+pico_emeraldWatch_1.change_wavelength_to("791.3")
