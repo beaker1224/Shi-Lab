f5d1b93f081c13d3be4c822d058b42f6e639d96a
beaker1224 <qs1213489684@gmail.com>
2024-08-15 17:38:23 -0700

    variable name editing

--- Shi/auto_lsm/pico_emeraldWatch_1.py
+++ Shi/auto_lsm/pico_emeraldWatch_1.py
@@ -106,13 +106,13 @@ def main():
     key_positions = {}
     if update_required:
 
-        input("Click on the wavelength setting, press 'enter' for next")
+        input("Hover your mouse on the wavelength setting, press 'enter' for next")
         for key in keys:
             key_position = get_key_position(key)
             key_name = f"wavelength_key_{key}"
             key_positions[key_name] = [key_position[0], key_position[1]]
 
-        input("Click on the power setting, press 'enter' for next")
+        input("Hover your mouse on the power setting, press 'enter' for next")
         for key in keys:
             key_position = get_key_position(key)
             key_name = f"power_key_{key}"
