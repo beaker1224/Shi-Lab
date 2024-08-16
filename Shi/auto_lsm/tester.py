913304efd0057ebd1809d67fb6b6a0de59895b8b
beaker1224 <qs1213489684@gmail.com>
2024-08-15 17:38:58 -0700

    tester

--- Shi/auto_lsm/tester.py
+++ Shi/auto_lsm/tester.py
@@ -5,6 +5,8 @@ import pico_emeraldWatch_1, FVWatch_2
 import time
 import json
 
+input("enter to test")
 pico_emeraldWatch_1.change_power_to("250")
 time.sleep(3)
 pico_emeraldWatch_1.change_wavelength_to("791.3")
+input("displaying error msg")
