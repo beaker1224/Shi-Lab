a43f95996a5adb9d5c1794d068b06eb8181cc47b
beaker1224 <qs1213489684@gmail.com>
2024-09-04 13:06:47 -0700

    Macros starter

---
+++ Shi/imageJ Macros/Open and subtract background.py
@@ -0,0 +1,6 @@
+  saveDir = currentDir.replace(srcDir, dstDir) if keepDirectories else dstDir
+  if not os.path.exists(saveDir):
+    os.makedirs(saveDir)
+  print "Saving to", saveDir
+  IJ.saveAs(imp, "Tiff", os.path.join(saveDir, fileName));
+  imp.close()
