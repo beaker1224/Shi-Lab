3c110e4d8d56db1f95c51becdf1b97441e4cd6b4
beaker1224 <qs1213489684@gmail.com>
2026-03-05 10:49:33 -0800

    update

--- Shi/Hongje/fitting_bg.py
+++ Shi/Hongje/fitting_bg.py
@@ -7,73 +7,81 @@ import csv
 
 # Please type the csv file name that will be processed with address
 # After processing, the 
+def fitted_bg_subtraction(filePath: str, inputFileType: str, outputFileType: str) -> None:
+    '''
+    This function performs background subtraction on a Raman spectrum by fitting a Gaussian curve to the background and subtracting it from the original spectrum.
+    Parameters:
+    filePath (str): The path to the input CSV file containing the Raman spectrum data.'''
+    Filename = filePath
 
-Filename = 'G:/Hongje/Shuo/Rapamycin/Raw/Rapa_D10/Gut/1_RAPA/RAPA_D10_RAPA_1'
+    range_start = 1000
+    range_end = 3900
+    if inputFileType == 'csv':
+        data = pd.DataFrame(pd.read_csv(Filename + '.csv', header=None)).to_numpy()
+    else:
+        data = pd.DataFrame(pd.read_csv(Filename + '.txt', delim_whitespace=True, header=None)).to_numpy()
 
-range_start = 1000
-range_end = 3900
+    axis_x = np.arange(range_start, range_end, 1)
 
-data = pd.DataFrame(pd.read_csv(Filename + '.csv')).to_numpy()
+    interp_data = interp1d(data[:, 0], data[:, 1])
 
-axis_x = np.arange(range_start, range_end, 1)
+    new_data = np.zeros([axis_x.shape[0], 2])
 
-interp_data = interp1d(data[:, 0], data[:, 1])
+    new_data[:, 0] = axis_x
+    new_data[:, 1] = interp_data(axis_x)
 
-new_data = np.zeros([axis_x.shape[0], 2])
-
-new_data[:, 0] = axis_x
-new_data[:, 1] = interp_data(axis_x)
-
-new_data[:, 1] = new_data[:, 1] - np.min(new_data[:, 1])
+    new_data[:, 1] = new_data[:, 1] - np.min(new_data[:, 1])
 
 
-def func(parm):
-    
-    wavenumber = new_data[2220:2800, 0]
-    inten = new_data[2220:2800, 1]
+    def func(parm):
+        
+        wavenumber = new_data[2220:2800, 0]
+        inten = new_data[2220:2800, 1]
+                    
+        fun = parm[0]*np.exp((-np.square(wavenumber-parm[1]))/(2*parm[2]*parm[2])) + parm[3]
                 
-    fun = parm[0]*np.exp((-np.square(wavenumber-parm[1]))/(2*parm[2]*parm[2])) + parm[3]
+        return np.mean(np.square(inten - fun))
+        
             
-    return np.mean(np.square(inten - fun))
-     
+            
+    init_val = [np.max(new_data[:, 1])/2, 3500, 100, np.min(new_data[:, 1])]
+            
+    lower_bounds = [0, 3300, 10, np.min(new_data[:, 1])]
+
+    upper_bounds = [np.max(new_data[:, 1]), 3600, 500, np.max(new_data[:, 1])]
+            
+    bounds = Bounds(lower_bounds, upper_bounds)
+            
+    result = minimize(func, init_val, method='Nelder-Mead', tol=1e-20, bounds=bounds)
+            
+            
+    res_parm = result.x
+
+
+    bg_curve = np.zeros([data.shape[0],])
+
+    bg_curve = res_parm[0]*np.exp((-np.square(new_data[:, 0]-res_parm[1]))/(2*res_parm[2]*res_parm[2])) + res_parm[3]
+
+    sub_data = new_data[:, 1] - bg_curve
+
+    sub_data = sub_data - np.mean(sub_data[1400:1500])
+
+
+
+    if outputFileType == 'csv':
+        f = open(Filename + '_spontaneous_Raman_BG_subtracted.csv', 'w', encoding='utf-8', newline='')
+    else:
+        f = open(Filename + '_spontaneous_Raman_BG_subtracted.txt', 'w', encoding='utf-8', newline='')
+
+    writer = csv.writer(f)
+
+    writer.writerow(['Wavenumber', 'Intensity', 'BG'])
+
+
+    for i in range(0, axis_x.shape[0]):
         
-        
-init_val = [np.max(new_data[:, 1])/2, 3500, 100, np.min(new_data[:, 1])]
-        
-lower_bounds = [0, 3300, 10, np.min(new_data[:, 1])]
-
-upper_bounds = [np.max(new_data[:, 1]), 3600, 500, np.max(new_data[:, 1])]
-        
-bounds = Bounds(lower_bounds, upper_bounds)
-        
-result = minimize(func, init_val, method='Nelder-Mead', tol=1e-20, bounds=bounds)
-        
-        
-res_parm = result.x
+        writer.writerow([new_data[i, 0], sub_data[i], bg_curve[i]])
 
 
-bg_curve = np.zeros([data.shape[0],])
-
-bg_curve = res_parm[0]*np.exp((-np.square(new_data[:, 0]-res_parm[1]))/(2*res_parm[2]*res_parm[2])) + res_parm[3]
-
-sub_data = new_data[:, 1] - bg_curve
-
-sub_data = sub_data - np.mean(sub_data[1400:1500])
-
-
-
-
-f = open(Filename + '_spontaneous_Raman_BG_subtracted.csv', 'w', encoding='utf-8', newline='')
-
-writer = csv.writer(f)
-
-writer.writerow(['Wavenumber', 'Intensity', 'BG'])
-
-
-for i in range(0, axis_x.shape[0]):
-    
-    writer.writerow([new_data[i, 0], sub_data[i], bg_curve[i]])
-
-
-f.close()
+    f.close()
 
